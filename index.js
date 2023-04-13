3d9d608c97a606816a8a853c179d715c52c59eea
Shiloh <shilohofulue17@gmail.com>
2023-04-13 20:17:05 +0100

    Refactored code for better readability

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const rock = document.querySelector('.rock')
-const paper = document.querySelector('.paper')
-const scissors = document.querySelector('.scissors')
-const info = document.querySelector('.info')
-const you = document.querySelector('.you')
-const computer = document.querySelector('.computer')
-let computerscore = 0
-let playerscore = 0
-const options = ['rock','paper','scissors']
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const info = document.querySelector('.info');
+const you = document.querySelector('.you');
+const computer = document.querySelector('.computer');
+let computerscore = 0;
+let playerscore = 0;
+const options = ['rock','paper','scissors'];
+
 
 function getComputerChoice(){
     return options[Math.floor(Math.random()*3)]
@@ -15,6 +16,7 @@ function getComputerChoice(){
 rock.addEventListener('click',function(){
     rps(p1='rock',getComputerChoice())
 })
+
 paper.addEventListener('click',function(){
     rps(p1='paper',getComputerChoice())
 })
@@ -33,7 +35,6 @@ const rps = (p1, p2) => {
       'paper': 'rock'
     };
 
-    
     rules[p1] === p2 ? you.innerText = `You:${' '+ ++playerscore}` : p1 === p2 ? 'Draw!' :  computer.innerText = `Computer:${' '+ ++computerscore}`
     rules[p1] === p2 ? info.innerText = 'You won!' : p1 === p2 ? info.innerText = 'Draw!' : info.innerText = 'Computer won!'
     return rules[p1] === p2 ? 'You won!' : p1 === p2 ? 'Draw!' : 'Computer won!';
